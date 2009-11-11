b6f2a369c769069b6e61a47b86c6fa59b9c23436
Antti <antti@rosvosektori.mossque.net>
2009-11-11 22:03:49 +0200

    Added rakkaus

---
+++ modules/rakkaus.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from ircbot import SingleServerIRCBot
+from irclib import nm_to_n, nm_to_h, irc_lower, ip_numstr_to_quad
+
+import hashlib
+
+def rakkaus(self, e, c):
+    nick = nm_to_n(e.source())
+    c = self.connection
+
+    line = e.arguments()[0]
+    target = "".join(line.split()[1:])
+    if (target and len(target) > 0):
+        m = hashlib.sha1()
+        m.update(nick)
+        m.update(target)
+        digest = m.digest()
+        score = 0
+        for i in range(0,len(digest)-1):
+            score += ord(digest[i])
+
+        c.privmsg(e.target(), "%s <3 %s: %d%%"%(nick, target, score % 101))
+
+rakkaus.commands = ['rakkaus']

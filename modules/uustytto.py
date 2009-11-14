b09ac27eb6c832bef3ca11f21b841937043b7a76
Paavo Pietarila <paavo@ishida>
2009-11-15 01:53:02 +0200

    edited uustytto.py, sql in

--- modules/uustytto.py
+++ modules/uustytto.py
@@ -15,11 +15,11 @@ def setup(self):
     self.joinhandlers.append(uustytto)
 
 def getlist(self):
-	cursor = self.db.cursor()
-	cursor.execute("""SELECT name FROM uustytto""")
-	for row in cursor.fetchall():
-		uustytto_lista.append(row[0])
-	
+    cursor = self.db.cursor()
+    cursor.execute("""SELECT name FROM uustytto""")
+    for row in cursor.fetchall():
+        uustytto_lista.append(row[0])
+
 def uustytto(self,e,c):
     
     nick = nm_to_n(e.source())
@@ -28,9 +28,9 @@ def uustytto(self,e,c):
     c.send_raw("WHOIS %s"%nick)
 
 def uustytto_callback(self,e,c):
-	if uustytto_lista == []:
-		getlist(self);
-		
+    if uustytto_lista == []:
+        getlist(self);
+
     nick = self.whoisinfo['user'][0]
     snick = re.sub('[^a-zA-Z0-9]','',nick)
     
@@ -41,8 +41,10 @@ def uustytto_callback(self,e,c):
     if string.lower(firstname) in girlnames :
         print " TYTTÖ! ----> %s"%realname
         if not snick in uustytto_lista:
-			cursor = self.db.cursor()
-			sqlquery = """INSERT INTO uustytto (NAME) VALUES (%s); """
-			cursor.execute(command, [snick] )
+            cursor = self.db.cursor()
+            sqlquery = """INSERT INTO uustytto (NAME) VALUES (%s); """
+            cursor.execute(sqlquery, [snick] )
             uustytto_lista.append(snick)
-			c.privmsg(e.target(), "UUSTYTTÖ <3")
+            c.privmsg(e.target(), "UUSTYTTÖ <3")
+        else:
+            # wanhatyttö

069d1d07a05b015fbfd358042bad46abbdbb5e9c
Paavo Pietarila <paavo@ishida>
2009-11-15 02:17:45 +0200

    edited uustytto.py

--- modules/uustytto.py
+++ modules/uustytto.py
@@ -7,6 +7,7 @@ import string
 from ircbot import SingleServerIRCBot
 from irclib import nm_to_n, nm_to_u
 
+from oksanen import hasSql
 from girlnames import girlnames
 
 uustytto_lista = []
@@ -15,10 +16,11 @@ def setup(self):
     self.joinhandlers.append(uustytto)
 
 def getlist(self):
-    cursor = self.db.cursor()
-    cursor.execute("""SELECT name FROM uustytto""")
-    for row in cursor.fetchall():
-        uustytto_lista.append(row[0])
+    if hasSql:
+        cursor = self.db.cursor()
+        cursor.execute("""SELECT name FROM uustytto""")
+        for row in cursor.fetchall():
+            uustytto_lista.append(row[0])
 
 def uustytto(self,e,c):
     
@@ -41,10 +43,15 @@ def uustytto_callback(self,e,c):
     if string.lower(firstname) in girlnames :
         print " TYTTÖ! ----> %s"%realname
         if not snick in uustytto_lista:
-            cursor = self.db.cursor()
-            sqlquery = """INSERT INTO uustytto (NAME) VALUES (%s); """
-            cursor.execute(sqlquery, [snick] )
+            if hasSql:
+                cursor = self.db.cursor()
+                sqlquery = """INSERT INTO uustytto (NAME) VALUES (%s); """
+                cursor.execute(sqlquery, [snick] )
             uustytto_lista.append(snick)
-            c.privmsg(e.target(), "UUSTYTTÖ <3")
+            print " - UUSTYTTÖ -"
+            print uustytto_lista
+            # c.privmsg(e.target(), "UUSTYTTÖ <3")
         else:
             # wanhatyttö
+            print " - WANHATYTTÖ -"
+            print uustytto_lista

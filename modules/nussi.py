918250c9a17c4faabe9c176e57827fa22a38eedb
Paavo Pietarila <paavo.pietarila@gmail.com>
2010-12-22 17:35:51 +0200

    Added time limits to nussi.py

--- modules/nussi.py
+++ modules/nussi.py
@@ -7,6 +7,8 @@ from irclib import nm_to_n, nm_to_h, irc_lower, ip_numstr_to_quad
 from censor import censor
 
 import random
+import time
+import math
 
 nusphrases = [ r"%s, %s olis vailla!", 
                r"%s, irstailu alkakoon, kumppaninasi %s.",
@@ -40,12 +42,48 @@ runkphrases = [ r"%s: fap fap fap fap fap fap FAP *GNUT*!",
                 r"Meni kuule sinulla %s runkkaus_hommiksi nyt!"
                 ]
 
+warnphrases = [ r"%s, rauhotuppa nyt vähäsen.",
+                r"%s, nyt annat kyllä muittenkin !nussia.",
+                r"%s, etkö saanut jo tarpeeksi? Nyt taisi olla sinun !nussimiset siinä joksikin aikaa.",
+                r"%s, näytänkö minä sinusta joltain parittajalta? Pidähän taukoa.",
+                r"%s, jos ei ehdotukset kelpaa niin yritähän itse tehdä omat päätöksesi. Nyt riitti hetkeksi."
+                ]
+
+"""tn_time is the time diff after which the prob of getting banned is zero"""
+nussi_tn_time = 2*60
+nussi_ban_time = 5*60
+
 def setup(self):
     self.pubcommands['nussi'] = nussi
+    nussi.warned = {}
+    nussi.last = {}
 
 def nussi(self, e, c):
     """ NUSNUS """
     nick = nm_to_n(e.source())
+
+    if nick in nussi.last :
+        """check if banned"""
+        if nussi.warned[nick] == 1 and (time.time() - nussi.last[nick]) < nussi_ban_time :
+            return
+        else :
+            nussi.warned[nick] = 0
+
+        tdiff = time.time() - nussi.last[nick]
+        nussi.last[nick] = time.time()
+        """!nussi too soon and you might get banned"""
+        ban_prob = 100*(1 - math.pow(tdiff/nussi_tn_time,0.5))
+        print ban_prob
+        if random.choice(range(100)) < ban_prob:
+            if not nick in nussi.warned or nussi.warned[nick] == 0 :
+                c.privmsg(e.target(), random.choice(warnphrases) % nick)
+                nussi.warned[nick] = 1
+                return
+            else : 
+                return
+    else :
+        nussi.last[nick] = time.time()
+        nussi.warned[nick] = 0
 
     for chname, chobj in self.channels.items():
         if e.target().lower() == chname.lower():

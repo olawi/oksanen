697aafca8dfa5dc059110021c0e52c06d183face
Paavo Pietarila <paavo.pietarila@gmail.com>
2014-11-04 14:05:21 +0200

    fixed a few bugs in sana.py

--- modules/sana.py
+++ modules/sana.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 from ircbot import SingleServerIRCBot
-from irclib import nm_to_n, nm_to_h, irc_lower, ip_numstr_to_quad
+from irclib import nm_to_n, nm_to_h, irc_lower, ip_numstr_to_quad, is_channel
 
 import string
 import random
@@ -36,13 +36,13 @@ def kysysana(self, e, c):
     line = e.arguments()[0]
     new_word = string.join(line.split()[1:], " ")
 
-    if is_admin(e.source()):
-        # admins can force a new !sana from wordlist or specify it
-        if len(new_word) > 0:
+    if (not is_channel(e.target()) and is_admin(e.source())):
+        # for admins in /query
+        if (len(new_word) > 0):
             sana.current_word = unicode(new_word, 'utf-8')
         else:
-        # users can ask Oksanen to repeat the !sana
             sana.current_word = random.choice(wordgame_wordlist)
+        sana.just_asked = False
 
     if (len(sana.current_word) > 0):
         self.cron.delete_event(sana.cron_id)
@@ -61,18 +61,19 @@ def sana(self):
     sana.current_word_shuffle = sana_shuffle(self, sana.current_word)
 
     output = "Ratkaise sana: %s"%(sana.current_word_shuffle)
-    c.privmsg(channel, output)
-    print "in module sana sending: %s / %s " % (sana.current_word_shuffle, sana.current_word)
-    sana.just_asked = True
+    if not sana.just_asked:
+        c.privmsg(channel, output)
+        print "in module sana sending: %s / %s " % (sana.current_word_shuffle, sana.current_word)
+        sana.just_asked = True
 
 def sanaChecker(self, e, c):
     sana.just_asked = False
-    if len(sana.current_word) != 0:
-        line = e.arguments()[0]
+    if len(sana.current_word) > 0:
+        line = unicode(e.arguments()[0], 'utf-8')
 
         if line == sana.current_word:
             nick = nm_to_n(e.source())
-            sana.current_word = None
+            sana.current_word = ""
             cursor = self.db.cursor()
             sqlquery = """INSERT INTO gamescores (user,wordgame) VALUES (%s,1) ON DUPLICATE KEY UPDATE wordgame = wordgame + 1;"""
             cursor.execute(sqlquery, [nick] )

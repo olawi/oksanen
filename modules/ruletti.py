7cdbf23aca35ae455920d4194dcb260b729bd1c5
Paavo Pietarila <paavo@harpo>
2015-03-04 18:04:57 +0200

    reverted to a working version of modules/ruletti.py

--- modules/ruletti.py
+++ modules/ruletti.py
@@ -3,7 +3,6 @@
 
 import random
 import time
-import numpy
 
 from ircbot import SingleServerIRCBot
 from irclib import nm_to_n, nm_to_h, irc_lower, ip_numstr_to_quad
@@ -51,9 +50,7 @@ def ruletti(self, e, c):
     if r == 1:
         c.kick(e.target(), nick, "*BANG*")
         c.mode(e.target(), "+b %s"%e.source())
-        d_mins = numpy.round(numpy.random.standard_gamma(1)*30)
-        t_kick = datetime.now() + timedelta (minutes = d_mins)
-        self.cron.add_event({'count':1, 'hour':[t_kick.hour], 'minute':[t_kick.mins]}, ruletti_unban, self, e, c)
+        self.cron.add_event({'count':1,'minute':[random.randint(0,59)]}, ruletti_unban, self, e, c)
     else:
         c.privmsg(e.target(),"%s, *click*"%nick)  
     

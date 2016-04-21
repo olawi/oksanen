24e3f1b5d7fa58305528c905b565afce0fbb173a
Paavo Pietarila <paavo.pietarila@gmail.com>
2016-04-21 12:52:10 +0300

    Faked user-agent for lintukoto.net

--- modules/oraakkeli.py
+++ modules/oraakkeli.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import urllib
+from urllib import FancyURLopener
 import htmllib
 import string
 from ircbot import SingleServerIRCBot
@@ -9,6 +9,9 @@ from irclib import nm_to_n, nm_to_h, irc_lower, ip_numstr_to_quad
 import ircutil
 
 oraakkeli_url = "http://www.lintukoto.net/viihde/oraakkeli/index.php?html=0&kysymys="
+
+class opener(FancyURLopener):
+    version = 'Mozilla/5.0 (X11; U; Linux x86_64; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.246.0 Safari/532.5'
 
 def setup(self):
     self.pubcommands['?'] = oraakkeli
@@ -27,7 +30,17 @@ def oraakkeli(self,e,c):
     # lintukoto expects latin-1
     query = ircutil.recode(query,'latin-1')
     
-    fd = urllib.urlopen("%s%s"%(oraakkeli_url,query))
+    opr = opener()
+    
+    uri = "%s%s"%(oraakkeli_url,query)
+
+    try:
+        fd = opr.open(uri)
+    except:
+        print "url.py: FAIL opening %s"%uri
+        c.privmsg(e.target(), "Sori, oraakkeli on nyt lomalla?")
+        return
+    
     reply = fd.read()
     fd.close
     
